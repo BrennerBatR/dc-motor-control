d27367246bc040daba109083e77fa1e12f35a5f3
Willian Gomes <will7seven@gmail.com>
2020-02-14 12:10:19 -0300

    adicionado leitura do adc

--- code/main.c
+++ code/main.c
@@ -2,21 +2,33 @@
 #include <stdint.h>
 #define  _XTAL_FREQ 4000000
 // globais
-
-int8_t global2 = 0;
-int16_t global3 = 0;
-uint8_t global4 = 0;
-int global1 = 0;
+uint16_t valor = 0;
 
 
 void main(void) {
-	TRISB = 0x00;
-	ANSEL = 0;
-	ANSELH = 0;
+	// configuração dos periféricos
+	//TRISA = TRISA | (1<<0); 
+	TRISA	|= (1<<0); // ra0 como entrada
+	ANSEL |= (1<<0); // ra0 como entrada analogica
+	// ADCON: ADCS = Fosc/8,CHS= AN0, GO = 0, ADON=1
+	ADCON0 = 0x00;
+	ADCON0 |= 0b01000001;
+	//ADCON1: ADFM= right, VCFG1= VSS, VCFG0= VDD
+	ADCON1 = 0;
+	ADCON1 |= (1<<7); 
+	TRISD	= 0;
+	TRISC	= 0;
+
 	
 	while(1){
-		PORTB = global4;
-		global4++;
-		__delay_ms(1000);
+		// loop infinito
+		ADCON0 |= (1<<1); // seta go/done (inicia conversão)
+		while( ADCON0 & (1<<1));
+		valor = (ADRESH<<8) + ADRESL;
+		TRISC = ADRESL;
+		TRISD = ADRESH;
+
+		
+	
 	}
 }
